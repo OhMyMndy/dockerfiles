d5e5c282183fffbac4ddc575b1a26c156ead9ce2
Mandy Schoep <mandyschoep@gmail.com>
2019-12-28 14:39:22 +0100

    Add Retroarch/Retropie setup scripts

--- dockerfiles/retropie/entrypoint.sh
+++ dockerfiles/retropie/entrypoint.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
 
 if [[ $INPUT_GROUP_ID != '' ]]; then
-    group_name="$(getent group $INPUT_GROUP_ID  | awk -F: '{print $1}')"
+    group_name="$(getent group "$INPUT_GROUP_ID"  | awk -F: '{print $1}')"
     if [[ $group_name != 'input' ]]; then
-        sudo groupmod --gid 992 $(getent group $INPUT_GROUP_ID  | awk -F: '{print $1}')
+        sudo groupmod --gid 992 "$(getent group "$INPUT_GROUP_ID"  | awk -F: '{print $1}')"
     fi
     sudo groupmod --gid "$INPUT_GROUP_ID"  input
 fi
 if [[ $PLUGDEV_GROUP_ID != '' ]]; then
-    group_name="$(getent group $PLUGDEV_GROUP_ID  | awk -F: '{print $1}')"
+    group_name="$(getent group "$PLUGDEV_GROUP_ID"  | awk -F: '{print $1}')"
     if [[ $group_name != 'plugdev' ]]; then
-        sudo groupmod --gid 992 $(getent group $PLUGDEV_GROUP_ID  | awk -F: '{print $1}')
+        sudo groupmod --gid 992 "$(getent group "$PLUGDEV_GROUP_ID"  | awk -F: '{print $1}')"
     fi
     sudo groupmod --gid "$PLUGDEV_GROUP_ID" plugdev
 fi
 
 
 mkdir -p "$HOME/.emulationstation/all"
-
+# shellcheck disable=SC1090
 source "$HOME/RetroPie-Setup/scriptmodules/inifuncs.sh"
+# shellcheck disable=SC1090
 source "$HOME/RetroPie-Setup/scriptmodules/helpers.sh"
+# shellcheck disable=SC1090
 source "$HOME/RetroPie-Setup/scriptmodules/emulators/retroarch.sh"
 
 addUdevInputRules
--- dockerfiles/retropie/install.sh
+++ dockerfiles/retropie/install.sh
@@ -2,5 +2,6 @@
 
 set -e
 for var in "$@"; do
+    # shellcheck disable=2086
     "$HOME/RetroPie-Setup/retropie_packages.sh" $var || exit 255
 done
